f0e6fb33d3075c507e80c69fc1e2d49bb5015994
swiftleah <leahrosehenry@gmail.com>
2023-11-24 02:18:40 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,3 @@
 #manifest file that installs flask from pip3
-package { 'python3-pip': ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0', 
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"', 
-  require => Package['python3-pip'], 
+package {'flask': ensure => '2.1.0', provider => 'pip3'
 }

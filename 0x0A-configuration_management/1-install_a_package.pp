d6c15ef88c0c5173c7c7f598182f4b572001cfc7
swiftleah <leahrosehenry@gmail.com>
2023-11-24 02:11:57 -0800

    Task 1 - for now

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+#manifest file that installs flask from pip3
+package { 'python3-pip': ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0', 
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"', 
+  require => Package['python3-pip'], 
+}

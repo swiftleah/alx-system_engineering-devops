bdb91f21523da6e004131eb841e3e92d1bb156cb
swiftleah <leahrosehenry@gmail.com>
2023-11-26 01:54:29 -0800

    Task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 #manifest file that changes our configuration file
+file { '/etc/ssh/ssh_config':
+ensure => present
+}
+
 file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',

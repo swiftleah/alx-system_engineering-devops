61b187cb5509cad038c8ebe2848a7202bb9ff3b8
swiftleah <leahrosehenry@gmail.com>
2024-01-02 06:22:54 -0800

    Task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pythons3
+""" this script exports in the JSON format """
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    URL = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(URL + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(URL + "todos", params={"userId": sys.argv[1]}).json()
+    user_id = sys.argv[1]
+    username = employee.get("username")
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+             "task": t.get("title"),
+             "completed": t.get("completed"),
+             "username": username
+            } for t in todo]}, jsonfile)
